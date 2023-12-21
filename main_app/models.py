3feb26d8ca1e838d6ff215560f0081790393e9ce
nholliday314 <nholliday314@gmail.com>
2023-12-21 18:00:47 -0500

    continued updates

---
+++ main_app/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.urls import reverse
+from datetime import date
+
+# A tuple of 2-tuples
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+  
+class Finch(models.Model):
+    name = models.CharField(max_length=50)
+    scientificname = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    mass = models.CharField(max_length=10)
+    diet = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+# Add new Feeding model below Cat model
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    # change the default sort
+    class Meta:
+        ordering = ['-date']
